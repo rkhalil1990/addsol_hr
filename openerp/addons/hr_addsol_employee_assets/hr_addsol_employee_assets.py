b287830ee03b6f5c5525f40b87a63e4213583a52
rupesh-addsol <rupeshg@aitspl.com>
2015-06-09 17:24:38 +0530

    return date should not appear when type=modify and request date is required

--- openerp/addons/hr_addsol_employee_assets/hr_addsol_employee_assets.py
+++ openerp/addons/hr_addsol_employee_assets/hr_addsol_employee_assets.py
@@ -33,13 +33,13 @@ class hr_addsol_employee(osv.osv):
 class hr_addsol_employee_assets(osv.osv):
     _name= "hr.addsol.employee.assets"
     _description="Employee request for assets"
-         
+    _rec_name = 'employee_id'    
 
     _columns = {
         'name':fields.text('Description', states={'draft': [('readonly', False)]} , readonly=True, required=True),
         'employee_id': fields.many2one('hr.employee', 'Employee', states={'draft': [('readonly', False)]} , readonly=True, required=True),
         'product_id': fields.many2one('product.product', 'Request For' ,states={'draft': [('readonly', False)]} ,readonly=True),
-        'date_from': fields.datetime('Request Date', states={'draft': [('readonly', False)]}, readonly=True),
+        'date_from': fields.datetime('Request Date', states={'draft': [('readonly', False)]}, required=True, readonly=True),
         'date_to': fields.datetime('End Date', states={'draft': [('readonly', False)]}, readonly=True),
         'quantity': fields.float('Quantity', states={'draft': [('readonly', False)]}, readonly=True),
         'state': fields.selection([('draft', 'To Submit'), ('cancel', 'Cancelled'),('confirm', 'To Approve'), ('validate', 'Approved'),('refuse', 'Refused')],
@@ -98,19 +98,23 @@ class hr_addsol_employee_assets(osv.osv):
         for prod_id in self.browse(cr, uid, ids):
             p_id = prod_id.product_id.id
             qty = prod_id.quantity  
-            if qty == 0:
-                    raise osv.except_osv(_('Error'), _('Quantity Can not be 0'))
-                         
-            destination_location = prod_id.product_id.property_stock_inventory.id
-            uom = prod_id.product_id.uom_id.id
-            prod_name = prod_id.product_id.name
-            location_obj = self.pool.get('stock.location')
-            source_location_ids = location_obj.search(cr,uid,[('name','=','Stock')])
-        
-            move_obj = self.pool.get('stock.move')
-            move_ids = move_obj.create(cr,uid,{'product_id':p_id,'name':prod_name,'product_uom_qty':qty,'product_uom':uom,'location_dest_id':destination_location,'location_id':source_location_ids[0]})
+            type = prod_id.type
             
-            move_obj.action_done(cr, uid, [move_ids])
+            if type=="asset":
+                
+                    if qty == 0:
+                        raise osv.except_osv(_('Error'), _('Quantity Can not be 0'))
+                    else:
+                        destination_location = prod_id.product_id.property_stock_inventory.id
+                        uom = prod_id.product_id.uom_id.id
+                        prod_name = prod_id.product_id.name
+                        location_obj = self.pool.get('stock.location')
+                        source_location_ids = location_obj.search(cr,uid,[('name','=','Stock')])
+                    
+                        move_obj = self.pool.get('stock.move')
+                        move_ids = move_obj.create(cr,uid,{'product_id':p_id,'name':prod_name,'product_uom_qty':qty,'product_uom':uom,'location_dest_id':destination_location,'location_id':source_location_ids[0]})
+                        
+                        move_obj.action_done(cr, uid, [move_ids])
            
         self.write(cr, uid, ids, {'state': 'validate'})
         return True
