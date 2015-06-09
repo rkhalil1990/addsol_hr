a221aea49f16a1454440170536a269368a5cfa82
rupesh-addsol <rupeshg@aitspl.com>
2015-06-09 14:31:34 +0530

    department field removed from expenses

--- openerp/addons/hr_addsol/__openerp__.py
+++ openerp/addons/hr_addsol/__openerp__.py
@@ -54,7 +54,7 @@ Few security rules for an employee:
     
 """,
     'images': [],
-    'depends': ['hr_holidays', 'hr_timesheet_sheet', 'l10n_in_hr_payroll', 'base_import_module'],
+    'depends': ['hr_holidays', 'hr_timesheet_sheet', 'l10n_in_hr_payroll', 'base_import_module', 'hr_expense'],
     'data': [
         'security/ir.model.access.csv',
         'security/ir_rule.xml',
